8101390093a9b1c7886c234ce647890e0e06682f
Dominik Schreiber <mail@dominikschreiber.de>
2022-09-29 19:44:02 +0200

    Properly spawn subprocesses and wait for them

--- mallob/setup_interface_at_client.sh
+++ mallob/setup_interface_at_client.sh
@@ -5,8 +5,13 @@ set -e
 source $(basename $0)/config.sh
 
 # Send files for job submission
-bash send_migrate.sh $client_job_submission_dir $mallob_hostname $port_m_jobsubmission
+bash send_migrate.sh $client_job_submission_dir $mallob_hostname $port_m_jobsubmission &
+pids="$pids $!"
 # Receive job results
-bash recv_migrate.sh $client_job_results_dir $port_c_jobresults
+bash recv_migrate.sh $client_job_results_dir $port_c_jobresults &
+pids="$pids $!"
 # Receive log directory
-bash recv_track.sh $client_overall_logdir $port_c_logoutput
+bash recv_track.sh $client_overall_logdir $port_c_logoutput &
+pids="$pids $!"
+
+wait $pids
--- mallob/setup_interface_at_mallob.sh
+++ mallob/setup_interface_at_mallob.sh
@@ -5,8 +5,13 @@ set -e
 source $(basename $0)/config.sh
 
 # Receive files for job submission
-bash recv_migrate.sh $mallob_job_submission_dir $port_m_jobsubmission
+bash recv_migrate.sh $mallob_job_submission_dir $port_m_jobsubmission &
+pids="$pids $!"
 # Send job results
-bash send_migrate.sh $mallob_job_results_dir $client_hostname $port_c_jobresults
+bash send_migrate.sh $mallob_job_results_dir $client_hostname $port_c_jobresults &
+pids="$pids $!"
 # Send log directory
-bash send_track.sh $mallob_overall_logdir $mallob_this_run_logdir $client_hostname $port_c_logoutput
+bash send_track.sh $mallob_overall_logdir $mallob_this_run_logdir $client_hostname $port_c_logoutput &
+pids="$pids $!"
+
+wait $pids
