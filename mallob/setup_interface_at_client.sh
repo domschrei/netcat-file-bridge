49d365c513cd435892efdabec24e2d1d6ef2307a
Dominik Schreiber <mail@dominikschreiber.de>
2022-09-30 09:17:51 +0200

    Fix port names

--- mallob/setup_interface_at_client.sh
+++ mallob/setup_interface_at_client.sh
@@ -6,13 +6,13 @@ bridgebasedir="$(dirname $0)/.."
 source $bridgebasedir/mallob/config.sh client
 
 # Send files for job submission
-$bridgebasedir/send_migrate.sh $client_job_submission_dir $port_m_jobsubmission $mallob_hostname &
+$bridgebasedir/send_migrate.sh $client_job_submission_dir $port_jobsubmission $mallob_hostname &
 pids="$pids $!"
 # Receive job results
-$bridgebasedir/recv_migrate.sh $client_job_results_dir $port_c_jobresults $mallob_hostname &
+$bridgebasedir/recv_migrate.sh $client_job_results_dir $port_jobresults $mallob_hostname &
 pids="$pids $!"
 # Receive log directory
-$bridgebasedir/recv_track.sh $client_overall_logdir $port_c_logoutput $mallob_hostname &
+$bridgebasedir/recv_track.sh $client_overall_logdir $port_logoutput $mallob_hostname &
 pids="$pids $!"
 
 wait $pids
--- mallob/setup_interface_at_mallob.sh
+++ mallob/setup_interface_at_mallob.sh
@@ -6,13 +6,13 @@ bridgebasedir="$(dirname $0)/.."
 source $bridgebasedir/mallob/config.sh mallob
 
 # Receive files for job submission
-$bridgebasedir/recv_migrate.sh $mallob_job_submission_dir $port_m_jobsubmission &
+$bridgebasedir/recv_migrate.sh $mallob_job_submission_dir $port_jobsubmission &
 pids="$pids $!"
 # Send job results
-$bridgebasedir/send_migrate.sh $mallob_job_results_dir $port_c_jobresults &
+$bridgebasedir/send_migrate.sh $mallob_job_results_dir $port_jobresults &
 pids="$pids $!"
 # Send log directory
-$bridgebasedir/send_track.sh $mallob_overall_logdir $mallob_this_run_logdir $port_c_logoutput &
+$bridgebasedir/send_track.sh $mallob_overall_logdir $mallob_this_run_logdir $port_logoutput &
 pids="$pids $!"
 
 wait $pids
